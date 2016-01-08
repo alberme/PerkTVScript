39c56afb2839268e4eaa40943feb79506d96ce65
albiejames <alberm2@uci.edu>
2016-01-08 06:35:47 -0800

    Update perktv.lua

--- perktv.lua
+++ perktv.lua
@@ -7,7 +7,7 @@
 PERKTV_ID = "com.jutera.perktv";
 errFlag, spriteNotFound, isVersion2, isVersion3 = false, false, false, false;
 -- parameters: table of colors, counts that should be attempted, table of coordinates to look in
-X_BUTTON = {{{11250603,0,0}, {11184810,29,0}, {15592941,0,-35}, {12303291,29,-35}, {11184810,15,-18}}, 1, nil;
+X_BUTTON = {{{11250603,0,0}, {11184810,29,0}, {15592941,0,-35}, {12303291,29,-35}, {11184810,15,-18}}, 1, nil};
 VIDEOS_BUTTON = {{{16330829,0,0}, {16264780,87,7}}, 1, nil};
 VIDEO_PANE1 = {{{7673895,0,0}, {12546172,5,1}, {8993862,3,8}, {5838374,-1,4}, {10369865,2,-4}}, 1, nil};
 VIDEO_PANE2 = {{{7368818,0,0}, {7367533,0,-59}, {6315873,54,-30}, {16316664,14,-29}}, 1, nil};
@@ -18,7 +18,7 @@ function checkVersion()
   if(getVersion() == "3.5.3-8") then
     log("Version 3.2.3-8");
     isVersionThree = true;
-  else if(getVersion() == "2.0.2") then
+  elseif(getVersion() == "2.0.2") then
     log("Version 2.0.2");
     isVersionTwo = true;
   else
@@ -49,7 +49,7 @@ function startApp()
       attempts = attempts + 1;
       usleep(6000000);
     end
-  else if(isVersionTwo) then
+  elseif(isVersionTwo) then
     while(findColor(13619151, 1, nil) == nil)
     do
       if(attempts == 3) then
@@ -102,13 +102,13 @@ function runVersionThree()
     log("Start timer, start playing videos");
   
     while(os.difftime(os.time(), playTime) < 400.0)
-      do
-        if(appState(PERKTV_ID) == "NOT RUNNING") then
-          log("Error: App has terminated unexpectedly");
-          break;
-        end
-      usleep(3000000);
+    do
+      if(appState(PERKTV_ID) == "NOT RUNNING") then
+        log("Error: App has terminated unexpectedly");
+        break;
       end
+    usleep(3000000);
+    end
   
     log(string.format("End video play time: %.2f seconds", os.difftime(os.time(), playTime)));
     log("Terminating Perk TV app");
@@ -162,6 +162,7 @@ function runVersionTwo()
     appKill(PERKTV_ID);
     usleep(2000000);
   end
+end
 
 --finds sprites on screen based on pixels and taps on them if found.
 function findAndTapSprite(sprite, name)
@@ -187,8 +188,8 @@ function findAndTapSprite(sprite, name)
         spriteNotFound = true;
         break;
       end
-        log(string.format("Couldn't find %s, making another attempt.", name));
-        attempts = attempts + 1;
+      log(string.format("Couldn't find %s, making another attempt.", name));
+      attempts = attempts + 1;
     end
   end
 end
@@ -212,12 +213,14 @@ do
     if(spriteNotFound) then
       log("Restarting app");
       appKill(PERKTV_ID);
-  else if(isVersionTwo) then
+    end
+  elseif(isVersionTwo) then
     log("Running version two function");
     runVersionTwo();
     if(spriteNotFound) then
       log("Restarting app");
       appKill(PERKTV_ID);
+    end
   end
   errFlag, spriteNotFound, isVersionTwo, isVersionThree = false;
 end
