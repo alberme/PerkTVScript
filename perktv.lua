e04862eede7dc54623e3436fe8a95744d6dda992
albiejames <alberm2@uci.edu>
2016-01-08 07:44:47 -0800

    Update perktv.lua

--- perktv.lua
+++ perktv.lua
@@ -1,11 +1,11 @@
 --construct algorithim for checking stuck videos
 
---AutoTouch script for Perk TV app
+--AutoTouch 3.5.3-8 script for Perk TV app. 
 --@author: Albert Martinez
 --Source: perktv.lua
 
 PERKTV_ID = "com.jutera.perktv";
-errFlag, spriteNotFound, isVersion2, isVersion3 = false, false, false, false;
+errFlag, spriteNotFound= false, false;
 -- parameters: table of colors, counts that should be attempted, table of coordinates to look in
 X_BUTTON = {{{11250603,0,0}, {11184810,29,0}, {15592941,0,-35}, {12303291,29,-35}, {11184810,15,-18}}, 1, nil};
 VIDEOS_BUTTON = {{{16330829,0,0}, {16264780,87,7}}, 1, nil};
@@ -16,14 +16,10 @@ GRAY = {13619151, 0, nil};
 --checks which version of AutoTouch is installed
 function checkVersion()
   if(getVersion() == "3.5.3-8") then
-    log("Version 3.2.3-8");
-    isVersionThree = true;
-  elseif(getVersion() == "2.0.2") then
-    log("Version 2.0.2");
-    isVersionTwo = true;
+    log("Version 3.5.3-8");
   else
     log("Unsupported version. Terminating script.");
-    alert("This script is not supported on the the version of AutoTouch you have installed. Terminating script.");
+    alert("This script is not supported on this version of AutoTouch you have installed. Terminating script.");
     errFlag = true;
   end
 end
@@ -36,37 +32,22 @@ function startApp()
   appRun(PERKTV_ID);
   usleep(6000000);
   
-  if(isVersionThree) then
-    while(appState(PERKTV_ID) == "NOT RUNNING")
-    do
-      if(attempts == 3) then
-        log("Perk TV failed to start, terminating script");
-        alert("Perk TV failed to start, maybe app is not installed?");
-        errFlag = true;
-      end
-      log("Perk TV did not open, attempting another run");
-      appRun(PERKTV_ID);
-      attempts = attempts + 1;
-      usleep(6000000);
+  while(appState(PERKTV_ID) == "NOT RUNNING")
+  do
+    if(attempts == 3) then
+      log("Perk TV failed to start, terminating script");
+      alert("Perk TV failed to start, maybe app is not installed?");
+      errFlag = true;
     end
-  elseif(isVersionTwo) then
-    while(findColor(13619151, 1, nil) == nil)
-    do
-      if(attempts == 3) then
-        log("Perk TV failed to start, terminating script");
-        alert("Perk TV failed to start, maybe app is not installed?");
-        errFlag = true;
-      end
-      log("Perk TV did not open, attempting another run");
-      appRun(PERKTV_ID);
-      attempts = attempts + 1;
-      usleep(6000000);
-    end
+    log("Perk TV did not open, attempting another run");
+    appRun(PERKTV_ID);
+    attempts = attempts + 1;
+    usleep(6000000);
   end
 end
 
---run function for iDevices running version 3
-function runVersionThree()
+--run function
+function run()
   adaptOrientation(ORIENTATION_TYPE.PORTRAIT);
 
   while(true)
@@ -117,53 +98,6 @@ function runVersionThree()
   end
 end
 
---run function for iDevices running version 2
-function runVersionTwo()
-  while(true)
-  do
-    startApp();
-    findAndTapSprite(X_BUTTON, "X button");
-    if(spriteNotFound) then
-      break;
-    end
-    
-    usleep(2000000);
-  
-    findAndTapSprite(VIDEOS_BUTTON, "Popular Videos");
-    if(spriteNotFound) then
-      break;
-    end
-
-    usleep(2000000);
-
-    findAndTapSprite(VIDEO_PANE1, "First Video Pane");
-    if(spriteNotFound) then
-      break;
-    end
-  
-    usleep(2000000);
-  
-    findAndTapSprite(VIDEO_PANE2, "First Video Button");
-    if(spriteNotFound) then
-      break;
-    end
-    
-    --playTime = os.time();
-    log("Start pause, start playing videos");
-  
-    --while(os.difftime(os.time(), playTime) < 400.0)
-    --do
-        --do some check for app crashed
-    --end
-    usleep(300000000);
-  
-    --log(string.format("End video play time: %.2f seconds", os.difftime(os.time(), playTime)));
-    log("Pause expired, terminating Perk TV app");
-    appKill(PERKTV_ID);
-    usleep(2000000);
-  end
-end
-
 --finds sprites on screen based on pixels and taps on them if found.
 function findAndTapSprite(sprite, name)
   local found, attempts = false, 0;
@@ -207,20 +141,10 @@ do
     return
   end
 
-  if(isVersionThree) then
-    log("Running version three function");
-    runVersionThree();
-    if(spriteNotFound) then
+  run();
+  if(spriteNotFound) then
       log("Restarting app");
       appKill(PERKTV_ID);
-    end
-  elseif(isVersionTwo) then
-    log("Running version two function");
-    runVersionTwo();
-    if(spriteNotFound) then
-      log("Restarting app");
-      appKill(PERKTV_ID);
-    end
   end
-  errFlag, spriteNotFound, isVersionTwo, isVersionThree = false;
+  errFlag, spriteNotFound = false;
 end
